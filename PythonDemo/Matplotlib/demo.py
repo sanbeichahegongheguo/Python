238686b7bafbcb5a24c470b27aba6cfb75f7b0d2
Volcano_ash <32977199+sanbeichahegongheguo@users.noreply.github.com>
2018-09-12 17:52:57 +0800

    修改样式

--- PythonDemo/Matplotlib/demo.py
+++ PythonDemo/Matplotlib/demo.py
@@ -4,11 +4,8 @@ Created on Wed Mar 14 10:13:32 2018
 
 @author: Administrator
 """
-
-
 import matplotlib.pyplot as plt
 from skimage import data
-img=data.astronaut()
+img = data.astronaut()
 plt.imshow(img)
 plt.show()
-
